377513b6a369e59ec66c6df456749c2649b916ac
dewmsr <kemisara.ana@student.mahidol.edu>
2025-03-30 17:09:35 +0700

    Add files via upload

---
+++ kemisara/lib/pages/medicine.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class TakeMedicinePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Take Medicine")),
+      body: Center(child: Text("This is the Take Medicine Page")),
+    );
+  }
+}
